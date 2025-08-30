1e54633e80f76ea330f448749a4f8789a1305fce
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-08-31 04:36:03 +0530

    `Added user_id, title, and content fields to noteSchema`

--- Backend/src/models/notes.js
+++ Backend/src/models/notes.js
@@ -2,6 +2,20 @@ import mongoose from 'mongoose';
 
 const noteSchema = new mongoose.Schema({
 
+    user_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Profile",
+        required: true
+    },
+    title:{
+        type: String,
+        required: true
+    },
+    content:{
+        type: String,
+    },
     
 
 }, { timestamps: true });
+
+export const Note = mongoose.model('Note', noteSchema);
