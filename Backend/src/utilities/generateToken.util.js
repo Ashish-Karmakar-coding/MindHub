9de1743543c8802e8d9f2086d06cdb78cb9c2e83
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-09-01 00:04:28 +0530

    some changes

--- Backend/src/utilities/generateToken.util.js
+++ Backend/src/utilities/generateToken.util.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const generateToken = (userId,res)=>{
     const token  = jwt.sign({userId},process.env.JWT_SECRET,{
-        expiresIn:"7d"
+        expiresIn: '7d',
     })
 
     res.cookie('jwt',token,{
