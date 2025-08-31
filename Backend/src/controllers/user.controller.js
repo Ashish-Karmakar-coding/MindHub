f314d701623252170fe32a68e923e7f50af07b58
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-08-31 13:45:17 +0530

    adding the routers

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,6 +1,27 @@
 const login = async (req, res) =>{
 
+}
+
+const signup = async(req,res)=>{
 
 }
 
-export {login};
+const logout= async (req,res)=>{
+
+}
+
+const updateProfile = async (req,res) =>{
+
+}
+
+const checkUser = async (req,res) =>{
+
+}
+
+export {
+    login,
+    signup,
+    logout,
+    updateProfile,
+    checkUser
+};
--- Backend/src/routes/users.route.js
+++ Backend/src/routes/users.route.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import {
-    login
+    login,
+    signup,
+    logout,
+    updateProfile,
+    checkUser,
 } from '../controllers/user.controller.js';
 
 const Router = express.Router();
 
 Router.post("/login",login)
+Router.post("/signup",signup)
+Router.get("/logout",logout)
+
+Router.put("/update",updateProfile)
+
+Router.get("/check-user",checkUser)
 
 export default Router;
