4ddde4c96f23e27763cc0dab0f6b796cd3832334
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-08-31 23:33:19 +0530

    add the login function

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -46,7 +46,40 @@ const signup = async (req, res) => {
   }
 };
 
-const login = async (req, res) => {};
+const login = async (req, res) => {
+
+    const {email,password} = req.body;
+    if(!email || !password){
+        return res.status(400).json({message:"All fields are required"})
+    }
+
+    try {
+        
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message:"User does not exist"})
+        }
+
+        const isPasswordCorrect = bcrypt.compare(password,user.password)
+        if(!isPasswordCorrect){
+            return res.status(400).json({message:"Invalid password"})
+        }
+
+        generateToken(user._id,res)
+        
+        return res.status(200).json({
+            message:"Login successful",
+            user:{
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        })
+
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Server Error" , error: error.message });
+    }
+};
 
 const logout = async (req, res) => {};
 
