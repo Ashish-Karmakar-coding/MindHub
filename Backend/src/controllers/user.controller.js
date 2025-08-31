7e11a418758d4f4e02f98dd5d49f763717078789
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-08-31 23:53:54 +0530

    add the check user function

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -120,6 +120,18 @@ const updateProfile = async (req, res) => {
     }
 };
 
-const checkUser = async (req, res) => {};
+const checkUser = async (req, res) => {
+    try {
+        const user = req.user
+        if(!user){
+            return res.status(400).json({message:"user not found"})
+        }
+    
+        return res.status(200).json(user)
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Server Error" , error: error.message });
+    }
+    
+};
 
 export { login, signup, logout, updateProfile, checkUser };
