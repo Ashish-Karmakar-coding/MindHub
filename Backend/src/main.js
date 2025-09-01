1df14ca89a850d4dce6f6233daec5c55c8ef2963
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-09-02 02:30:54 +0530

    `feat: updated Backend/src/main.js with new import and app configuration changes`

--- Backend/src/main.js
+++ Backend/src/main.js
@@ -3,19 +3,21 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import userRouter from './routes/users.route.js';
+import { connectDB } from './database/connectDB.js';
 
 
 dotenv.config();
 
 const app = express();
-app.use(cookieParser());
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/users",userRouter)
 
 
 app.listen(PORT,()=>{
+    connectDB();
     console.log(`Server is running on port ${PORT}....`);
 })
