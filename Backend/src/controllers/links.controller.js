c9de99bf37b3ccb64ecec69f023fd0849e81445b
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-09-02 03:13:11 +0530

    add the get links functionality

--- Backend/src/controllers/links.controller.js
+++ Backend/src/controllers/links.controller.js
@@ -32,6 +32,21 @@ const addLink = async (req,res) =>{
 
 const getLinks = async (req,res) =>{
 
+    const userId = req.user._id
+    try {
+
+        const links = await Link.find({user_id:userId}).sort({createdAt: -1});
+
+        if (!links) {
+            return res.status(404).json({ message: 'No links found' });
+        }
+
+        res.status(200).json(links)
+        
+    } catch (error) {
+        return res.status(500).json({message: 'Internal Server Error',})
+    }
+
 }
 
 export {addLink,getLinks};
