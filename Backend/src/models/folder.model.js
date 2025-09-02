c1ba2075a62889d48630919b3a2bcb2b0958ace5
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-09-02 23:23:18 +0530

    add the folder model

---
+++ Backend/src/models/folder.model.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const folderSchema = new mongoose.Schema({
+
+    user_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    name:{
+        type: String,
+        required: true
+    },
+    files:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'File'
+    }]
+
+},{timestamps: true});
+
+export const Folder = mongoose.model('Folder', folderSchema);
