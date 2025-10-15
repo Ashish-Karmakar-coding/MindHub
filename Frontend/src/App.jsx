ebed573c223012fb6183c1f022645ab0935d3fed
Ashish Karmakar <kenshin8167@gmail.com>
2025-10-16 00:50:44 +0530

    fix some problems

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {Routes , Route, Navigate} from "react-router-dom";
 import './App.css'
+
+import { useEffect } from 'react'
 import Signup from './pages/Signup.page.jsx'
 import Login from './pages/Login.page.jsx'
 import Navbar from './components/navbar.jsx'
@@ -7,11 +9,17 @@ import Sidebar from './components/sidebar.jsx'
 import Dashboard from './pages/Dashboard.page.jsx'
 import Notes from './pages/Notes.page.jsx'
 
-  import {authUser} from './lib/authStore.js'
+  import { useAuthStore } from './lib/authStore.js'
 
 function App() {
+
+   const {authUser,checkAuth , isCheckingAuth ,onlineUsers} = useAuthStore();
+  useEffect(() => {
+    checkAuth()
+  },[]); 
+
   return (
-    <Router>
+
 
       <Routes>
         <Route path="/" element={authUser ? <Dashboard />: <Navigate to="/signup"/>} />
@@ -19,15 +27,14 @@ function App() {
         <Route path="/signup" element={!authUser ? <Signup /> : <Navigate to="/"/>} />
         <Route path="/notes" element={authUser ? <Notes />: <Navigate to="/signup"/>} />
         {/* <Route path="/profile" element={authUser ? <ProfilePage />: <Navigate to="/signup"/>} /> */}
-      </Routes>
       {/* <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/notes" element={<Notes />} />
         <Route path="/" element={<Dashboard />} />
-      </Routes> */}
-    </Router>
+        </Routes> */}
+        </Routes>
   )
 }
 
