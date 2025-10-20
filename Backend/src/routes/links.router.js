885077ec27c4ea290881a1260656def62443f8de
Ashish Karmakar <kenshin8167@gmail.com>
2025-10-20 23:56:31 +0530

    add the links

--- Backend/src/routes/links.router.js
+++ Backend/src/routes/links.router.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {
     addLink,
     getLinks,
+    deleteLink 
 } from '../controllers/links.controller.js';
 import {tokenCheck} from '../middlewares/tokenCheck.middleware.js'
 
@@ -10,5 +11,7 @@ const router = express.Router();
 router.post("/add-link",tokenCheck,addLink)
 
 router.get("/get-link",tokenCheck,getLinks)
+
+router.delete("/delete-link",tokenCheck,deleteLink)
 
 export default router;
