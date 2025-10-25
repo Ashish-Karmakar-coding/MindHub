626b73fcd2c145dda77d4e83bae951e4ca2c262c
Ashish Karmakar <kenshin8167@gmail.com>
2025-10-25 13:19:05 +0530

    some changes

--- Backend/src/utils/generateToken.util.js
+++ Backend/src/utils/generateToken.util.js
@@ -9,7 +9,7 @@ export const generateToken = (userId,res) => {
     })
 
     res.cookie('jwt', token,{
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        maxAge: 12 * 24 * 60 * 60 * 1000, // 12 days
         httpOnly: true,
         sameSite: 'Strict',
         secure : process.env.NODE_ENV !== 'development', // Use secure cookies in production
