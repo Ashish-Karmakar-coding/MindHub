740fcc0b59c1535c2c1aec1945c3172857bb89bf
Ashish Karmakar <kenshin8167@gmail.com>
2025-12-01 13:45:03 +0530

    someee changes

--- Backend/src/main.js
+++ Backend/src/main.js
@@ -51,10 +51,10 @@ app.use("/api/files",fileRouter)
 app.use("/api/folders",folderRouter)
 
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, '../../Frontend/dist')))
+  app.use(express.static(path.join(__dirname, '../frontend/dist')))
   // ✅ Fixed: Added parameter name to wildcard route
-  app.get('/files{/*path}', (req, res) => {
-      res.sendFile(path.join(__dirname, "../","../Frontend","dist","index.html"));
+  app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
   });
 }
 
